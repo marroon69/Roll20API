06fc0754b00adb78405790cdad35b5a5717cb44e
marroon69 <35424343+marroon69@users.noreply.github.com>
2020-05-17 12:10:54 -0500

    Update savageworldsroller.js
    Cleaned up the code

--- savageworldsroller.js
+++ savageworldsroller.js
@@ -1,6 +1,6 @@
 on("chat:message", function(msg) {
     
-    // Retrieves tjhe Token name form the seleected tokens
+    // Retrieves the Token name form the seleected tokens
     function getTokenName(selected) {
         
         var name = '';
@@ -9,14 +9,14 @@ on("chat:message", function(msg) {
             var token = getObj('graphic',selected_id);
            
             if(token) {
-                log(token.get('name'))    
+
                 name = token.get('name');
             }
         }
         return name;
     }
   if(msg.type == "api" && msg.content.indexOf("!swr ") !== -1) {
-    log(msg.content);
+    
     //constant for dice allowed
     var allowedDice=['4','6','8','10','12'];
     var errorMessage="&{template:default}{{name=Savage Worlds Roller Doc}}{{doc=please use following format !swr trait #d#+/-#}}{{API command=!SWR}}{{trait=the trait you want to roll (ie Agility, Shooting)}}{{First #=number of dice (>0 but <7)}}{{Second #=The die type (4,6,8,10,12)}}{{Optional Third #=Add the bonus/penalty to the roll (ie 2 or-2)}}";
@@ -45,30 +45,30 @@ on("chat:message", function(msg) {
     }
     //Validate Inputs
     
-     if (!traitName || !numberofDice || isNaN(numberofDice) || numberofDice<1 || numberofDice > 5 ||!diceSize||!allowedDice.includes(diceSize)||isNaN(bonus)) {
+     if (!traitName || !numberofDice || isNaN(numberofDice) || numberofDice<1 || numberofDice > 5 || !diceSize || !allowedDice.includes(diceSize) || isNaN(bonus)) {
        sendChat(msg.who, errorMessage);
        return;
     }
     
     //Build Template
     var template = "&{template:default}"; 
-    var name="{{name=";
+    var name = "{{name=";
     var openBracket = "{{";
     var closeBracket = "}}";
-    var trait = "[[1d"+diceSize+"!+"+bonus+"]]}}";
-    var wildDie = "{{Wild Die=[[1d6!"+"+"+bonus+"]]}}";
+    var trait = "[[1d" + diceSize + "!+" + bonus + "]]}}";
+    var wildDie = "{{Wild Die=[[1d6!" + "+" + bonus + "]]}}";
     var token_name = getTokenName(msg.selected);
     if(!token_name) {
         token_name = msg.who;
     }
-    var rollTemplate = template+name+token_name+" Rolls "+traitName+closeBracket;
+    var rollTemplate = template + name + token_name + " Rolls " + traitName + closeBracket;
     if (numberofDice <= 1){
-        rollTemplate = rollTemplate + openBracket +traitName+" die " + "=" +trait;
+        rollTemplate = rollTemplate + openBracket + traitName +" die " + "=" + trait;
     } else {
         var i=0;
         for (i = 0; i < numberofDice; i++) {
             var rollNumer = Number(i) + 1;
-            rollTemplate = rollTemplate + openBracket +traitName+" die " + rollNumer + "=" +trait;
+            rollTemplate = rollTemplate + openBracket + traitName + " die " + rollNumer + "=" +trait;
         }
     }
     rollTemplate = rollTemplate+wildDie;
